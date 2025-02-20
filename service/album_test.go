86cb0b76384592082c79e5cbed35d22fe15ce49e
huavcjj <huavcjj0217@icloud.com>
2025-02-20 16:07:30 +0900

    test: service層の単体テストを実装

---
+++ service/album_test.go
@@ -0,0 +1,132 @@
+package service_test
+
+import (
+	"context"
+	"github.com/pulse227/server-recruit-challenge-sample/model"
+	"github.com/pulse227/server-recruit-challenge-sample/service"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type MockAlbumRepository struct {
+	mock.Mock
+}
+
+func NewMockAlbumRepository() *MockAlbumRepository {
+	return &MockAlbumRepository{}
+}
+
+func (m *MockAlbumRepository) GetAll(ctx context.Context) ([]*model.Album, error) {
+	args := m.Called(ctx)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).([]*model.Album), args.Error(1)
+}
+func (m *MockAlbumRepository) Get(ctx context.Context, id model.AlbumID) (*model.Album, error) {
+	args := m.Called(ctx, id)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*model.Album), args.Error(1)
+}
+func (m *MockAlbumRepository) Add(ctx context.Context, album *model.Album) error {
+	args := m.Called(ctx, album)
+	return args.Error(0)
+}
+func (m *MockAlbumRepository) Delete(ctx context.Context, id model.AlbumID) error {
+	args := m.Called(ctx, id)
+	return args.Error(0)
+}
+
+type AlbumServiceSuite struct {
+	suite.Suite
+	albumService        service.AlbumService
+	mockAlbumRepository *MockAlbumRepository
+}
+
+func TestAlbumServiceTestSuite(t *testing.T) {
+	suite.Run(t, new(AlbumServiceSuite))
+}
+
+func (suite *AlbumServiceSuite) SetupSuite() {
+	suite.mockAlbumRepository = NewMockAlbumRepository()
+	suite.albumService = service.NewAlbumService(suite.mockAlbumRepository)
+}
+
+func (suite *AlbumServiceSuite) TestAlbumServiceGetAlbumListService() {
+	ctx := context.Background()
+
+	albums := []*model.Album{
+		{
+			ID:       model.AlbumID(1),
+			Title:    "First Album",
+			SingerID: model.SingerID(1),
+			Singer:   &model.Singer{ID: model.SingerID(1), Name: "Test Singer"},
+		},
+		{
+			ID:       model.AlbumID(2),
+			Title:    "Second Album",
+			SingerID: model.SingerID(1),
+			Singer:   &model.Singer{ID: model.SingerID(1), Name: "Test Singer"},
+		},
+	}
+
+	suite.mockAlbumRepository.On("GetAll", ctx).Return(albums, nil)
+
+	result, err := suite.albumService.GetAlbumListService(ctx)
+
+	suite.Assert().Nil(err)
+	suite.Assert().Equal(albums, result)
+	suite.mockAlbumRepository.AssertExpectations(suite.T())
+}
+
+func (suite *AlbumServiceSuite) TestAlbumServiceGetAlbumService() {
+	ctx := context.Background()
+
+	album := &model.Album{
+		ID:       model.AlbumID(1),
+		Title:    "First Album",
+		SingerID: model.SingerID(1),
+		Singer:   &model.Singer{ID: model.SingerID(1), Name: "Test Singer"},
+	}
+
+	suite.mockAlbumRepository.On("Get", ctx, album.ID).Return(album, nil)
+
+	result, err := suite.albumService.GetAlbumService(ctx, album.ID)
+
+	suite.Assert().Nil(err)
+	suite.Assert().Equal(album, result)
+	suite.mockAlbumRepository.AssertExpectations(suite.T())
+}
+
+func (suite *AlbumServiceSuite) TestAlbumServicePostAlbumService() {
+	ctx := context.Background()
+
+	album := &model.Album{
+		ID:       model.AlbumID(1),
+		Title:    "New Album",
+		SingerID: model.SingerID(1),
+		Singer:   &model.Singer{ID: model.SingerID(1), Name: "Test Singer"},
+	}
+
+	suite.mockAlbumRepository.On("Add", ctx, album).Return(nil)
+
+	err := suite.albumService.PostAlbumService(ctx, album)
+
+	suite.Assert().Nil(err)
+	suite.mockAlbumRepository.AssertExpectations(suite.T())
+}
+
+func (suite *AlbumServiceSuite) TestAlbumServiceDeleteAlbumService() {
+	ctx := context.Background()
+	id := model.AlbumID(1)
+
+	suite.mockAlbumRepository.On("Delete", ctx, id).Return(nil)
+
+	err := suite.albumService.DeleteAlbumService(ctx, id)
+
+	suite.Assert().Nil(err)
+	suite.mockAlbumRepository.AssertExpectations(suite.T())
+}
---
+++ service/singer_test.go
@@ -0,0 +1,110 @@
+package service_test
+
+import (
+	"context"
+	"github.com/pulse227/server-recruit-challenge-sample/model"
+	"github.com/pulse227/server-recruit-challenge-sample/service"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type MockSingerRepository struct {
+	mock.Mock
+}
+
+func NewMockSingerRepository() *MockSingerRepository {
+	return &MockSingerRepository{}
+}
+
+func (m *MockSingerRepository) GetAll(ctx context.Context) ([]*model.Singer, error) {
+	args := m.Called(ctx)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).([]*model.Singer), args.Error(1)
+}
+func (m *MockSingerRepository) Get(ctx context.Context, id model.SingerID) (*model.Singer, error) {
+	args := m.Called(ctx, id)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*model.Singer), args.Error(1)
+}
+func (m *MockSingerRepository) Add(ctx context.Context, singer *model.Singer) error {
+	args := m.Called(ctx, singer)
+	return args.Error(0)
+}
+func (m *MockSingerRepository) Delete(ctx context.Context, id model.SingerID) error {
+	args := m.Called(ctx, id)
+	return args.Error(0)
+}
+
+type SingerServiceSuite struct {
+	suite.Suite
+	singerService        service.SingerService
+	mockSingerRepository *MockSingerRepository
+}
+
+func TestSingerServiceTestSuite(t *testing.T) {
+	suite.Run(t, new(SingerServiceSuite))
+}
+
+func (suite *SingerServiceSuite) SetupSuite() {
+	suite.mockSingerRepository = NewMockSingerRepository()
+	suite.singerService = service.NewSingerService(suite.mockSingerRepository)
+}
+
+func (suite *SingerServiceSuite) TestSingerServiceGetSingerListService() {
+	ctx := context.Background()
+
+	singers := []*model.Singer{
+		{ID: model.SingerID(1), Name: "Test Singer 1"},
+		{ID: model.SingerID(2), Name: "Test Singer 2"},
+	}
+	suite.mockSingerRepository.On("GetAll", ctx).Return(singers, nil)
+
+	result, err := suite.singerService.GetSingerListService(ctx)
+	suite.Assert().Nil(err)
+	suite.Assert().Equal(singers, result)
+	suite.Assert().Equal(len(singers), len(result))
+	suite.Assert().Equal(singers[0].ID, result[0].ID)
+	suite.Assert().Equal(singers[0].Name, result[0].Name)
+	suite.Assert().Equal(singers[1].ID, result[1].ID)
+	suite.Assert().Equal(singers[1].Name, result[1].Name)
+	suite.mockSingerRepository.AssertExpectations(suite.T())
+}
+
+func (suite *SingerServiceSuite) TestSingerServiceGetSingerService() {
+	ctx := context.Background()
+
+	singer := &model.Singer{ID: model.SingerID(1), Name: "Test Singer"}
+	suite.mockSingerRepository.On("Get", ctx, singer.ID).Return(singer, nil)
+
+	result, err := suite.singerService.GetSingerService(ctx, singer.ID)
+	suite.Assert().Nil(err)
+	suite.Assert().Equal(singer, result)
+	suite.mockSingerRepository.AssertExpectations(suite.T())
+}
+
+func (suite *SingerServiceSuite) TestSingerServicePostSingerService() {
+	ctx := context.Background()
+
+	singer := &model.Singer{ID: model.SingerID(1), Name: "Test Singer"}
+	suite.mockSingerRepository.On("Add", ctx, singer).Return(nil)
+
+	err := suite.singerService.PostSingerService(ctx, singer)
+	suite.Assert().Nil(err)
+	suite.mockSingerRepository.AssertExpectations(suite.T())
+}
+
+func (suite *SingerServiceSuite) TestSingerServiceDeleteSingerService() {
+	ctx := context.Background()
+
+	id := model.SingerID(1)
+	suite.mockSingerRepository.On("Delete", ctx, id).Return(nil)
+
+	err := suite.singerService.DeleteSingerService(ctx, id)
+	suite.Assert().Nil(err)
+	suite.mockSingerRepository.AssertExpectations(suite.T())
+}
